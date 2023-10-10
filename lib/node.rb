1fd30e20fa6a19127b2534f3e2dc8cdfe96b4abd
Blake <blameswalker@gmail.com>
2023-10-10 13:38:19 -0700

    Include Comparable module in Node class and modify to compare two nodes' values quickly

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,15 @@
 class Node
   attr_accessor :value, :left_child, :right_child
 
+  include Comparable
+
   def initialize(value)
     @value = value
     @left_child = nil
     @right_child = nil
+  end
+
+  def <=>(other_node)
+    @value <=> other_node.value
   end
 end

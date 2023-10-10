04a467507463081b112be2474b344029330a9c42
Blake <blameswalker@gmail.com>
2023-10-10 14:01:15 -0700

    Implement #insert method in Tree class to add new values to existing tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -9,8 +9,29 @@ class Tree
 
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
-    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
+  end
+
+  def insert(value, node = @root)
+    insertion = Node.new(value)
+
+    case insertion <=> node
+    when -1
+      if node.left_child.nil?
+        node.left_child = insertion
+      else
+        insert(value, node.left_child)
+      end
+    when 0
+      puts "Value already exists in the tree."
+    when 1
+      if node.right_child.nil?
+        node.right_child = insertion
+      else
+        insert(value, node.right_child)
+      end
+    end
   end
 
   private
--- main.rb
+++ main.rb
@@ -1,7 +1,18 @@
 require_relative 'lib/tree.rb'
 
 test_array = Array.new(20).map { |data| data = (rand * 100).to_i }
+p test_array
+
+gets
 
 tree = Tree.new(test_array)
+
+tree.pretty_print
+
+gets
+
+puts "Enter insert value:"
+tree.insert(73)
+tree.insert(74)
 
 tree.pretty_print

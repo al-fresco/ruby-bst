440e19c8f9aab4b1964ec9d60050cb4dc64de9d9
Blake <blameswalker@gmail.com>
2023-10-12 01:56:04 -0700

    Implement #level_order instance method in Tree class

--- lib/tree.rb
+++ lib/tree.rb
@@ -62,6 +62,20 @@ class Tree
     node
   end
 
+  def level_order(queue = [@root], visited = [], &block)
+    if queue.empty?
+      if block_given?
+        visited.each { |node| block.call(node) }
+      else
+        visited
+      end
+    else
+      node = queue.shift
+
+      level_order(queue.push(node.left, node.right).compact, visited.push(node), &block)
+    end
+  end
+
   private
 
   def build_tree(array)
--- main.rb
+++ main.rb
@@ -3,11 +3,20 @@
 require_relative 'lib/tree'
 require_relative 'lib/helpers'
 
-tree = Tree.create_random(20)
+# Creates tree; prints tree
+tree = Tree.create_random(11)
 tree.pretty_print
 
+# Inserts 3 into the tree; prints tree
 tree.insert(3)
-tree.insert(10)
-tree.insert(15)
-
 tree.pretty_print
+
+# Returns specified node
+tree.find(3)
+
+# Deletes inserted value from the tree; prints tree
+tree.delete(3)
+tree.pretty_print
+
+# Passes nodes to a block in level order
+tree.level_order { |node| print "#{node.data} " }
